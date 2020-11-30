7d4735b1fee235a6aa32215250abcd44efced1b4
pavithra <pavi6051@gmail.com>
2020-11-30 11:37:29 +0530

    Second Commit

---
+++ frontend/gui/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import BaseRouter from './routes';
+//import logo from './logo.svg';
+
+import 'antd/dist/antd.css';
+
+import CustomLayout from './containers/Layout';
+
+class App extends Component {
+  render() { 
+  return (
+    <div className="App">
+      <Router>
+      <CustomLayout>
+        <BaseRouter />
+      
+      </CustomLayout>
+
+      </Router>
+      
+    </div>
+  );
+}
+}
+
+export default App;
